044563b9c8cec072bef4b38ffd0586a5d2fd6983
Kohki Mori <Kohki Mori>
2017-03-13 10:40:30 +0900

    respond to orientation change

---
+++ app/src/main/java/com/example/kohki/tocostickapp/ChartMenuFragment.java
@@ -0,0 +1,26 @@
+package com.example.kohki.tocostickapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Kohki on 2017/03/12.
+ */
+
+public class ChartMenuFragment  extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_graph_menu, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
